2d4d783990818ad0cddf83bf55b8f1767cc9b8b8
peparhugo <peparhugo@gmail.com>
2021-05-25 17:04:13 -0600

    chore: updated requests to use parameters

--- scripts/stats_can_data.py
+++ scripts/stats_can_data.py
@@ -9,35 +9,57 @@ class StatsCanData:
 
     def get_census_tracts(self, geos='CT', cpt='47'):
         data = requests.get(
-            'https://www12.statcan.gc.ca/rest/census-recensement/CR2016Geo.json?lang=E&geos={}&cpt={}'.format(geos,
-                                                                                                              cpt),
-            verify=False)
+            'https://www12.statcan.gc.ca/rest/census-recensement/CR2016Geo.json',
+            params=dict(
+                lang='E',
+                geos=geos,
+                cpt=cpt
+            )
+        )
         data_dict = json.loads(data.content.decode().replace('//', ''))
         df = pd.DataFrame(data_dict['DATA'], columns=data_dict['COLUMNS'])
         return df
 
     def get_census_tract_geometry(self, dguid):
         data = requests.get(
-            'https://geoprod.statcan.gc.ca/arcgis/rest/services/MD2DM2016_2021/MapServer/11/query?f=json&where=DGuid%20in%20(%27{}%27)&returnGeometry=true&spatialRel=esriSpatialRelIntersects&outFields=*&outSR=4326'.format(
-                dguid), verify=False)
+            'https://geoprod.statcan.gc.ca/arcgis/rest/services/MD2DM2016_2021/MapServer/11/query',
+            params=dict(f='json',
+                        where="DGuid in ('{}')".format(dguid),
+                        returnGeometry=True,
+                        spatialRel='esriSpatialRelIntersects',
+                        outFields='*',
+                        outSR=4326)
+        )
         print('Got geometry ', dguid)
         return shapely.geometry.Polygon(data.json()['features'][0]['geometry']['rings'][0])
 
     def get_census_tracts_data(self, dguid):
         data = requests.get(
-            'https://www12.statcan.gc.ca/rest/census-recensement/CPR2016.json?lang=E&dguid={}'.format(dguid),
-            verify=False)
+            'https://www12.statcan.gc.ca/rest/census-recensement/CPR2016.json',
+            params=dict(
+                lang='E',
+                dguid=dguid
+            )
+        )
         print('Got stats ', dguid)
         return pd.DataFrame(json.loads(data.content.decode().replace('//', ''))['DATA'],
                             columns=json.loads(data.content.decode().replace('//', ''))['COLUMNS'])
 
-    def get_stats_can_data(self):
+    def get_stats_can_data(self, target_geo_uid):
         census_tracts = self.get_census_tracts()
         print('Got census tracts')
-        self.census_tracts_geo = gpd.GeoDataFrame(census_tracts,
-                                                  geometry=census_tracts.GEO_UID.map(
-                                                      self.get_census_tract_geometry).tolist())
-        print('Got cenesus tracts geometery')
-        self.census_tracts_stats = pd.concat(census_tracts.GEO_UID.map(self.get_census_tracts_data).tolist(), axis=0)
+        self.census_tracts_geo = gpd.GeoDataFrame(
+            census_tracts[
+                census_tracts.GEO_UID.isin(target_geo_uid)
+            ],
+            geometry=census_tracts[
+                census_tracts.GEO_UID.isin(target_geo_uid)
+            ].GEO_UID.map(self.get_census_tract_geometry).tolist()
+        )
+        print('Got census tracts geometery')
+        self.census_tracts_stats = pd.concat(
+            census_tracts[census_tracts.GEO_UID.isin(target_geo_uid)].GEO_UID.map(self.get_census_tracts_data).tolist()
+            , axis=0
+        )
         print('Got census tracts stats')
         self.census_tracts_geo.crs = {'init': 'epsg:4326'}
